3dc4d8a8955e66f86a3cb5c9e8ea6a21d0b264e6
Tomasz Gil <tomaszgil@hotmail.com>
2016-11-28 14:27:24 +0100

    Change names of instance files

--- scripts/instancegenerator_test.py
+++ scripts/instancegenerator_test.py
@@ -3,7 +3,7 @@ from instancegenerator import InstanceGenerator
 
 generator = InstanceGenerator(50, 1000, 1, 1000)
 generator.generateInstance()
-generator.save("regular_instance")
+generator.save("regular-instance")
 
 generator.generateOptimumInstance()
-generator.save("optimum_instance")
+generator.save("optimum-instance")
