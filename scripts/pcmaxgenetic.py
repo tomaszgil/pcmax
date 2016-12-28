fd8e06a3bb44031f273ab3e5948989028c5dd2f8
Tomasz Gil <tomaszgil@hotmail.com>
2016-12-28 13:38:35 +0100

    Create brief structure of a genetic algorithm, implement choosing the best greedy solution

--- scripts/pcmaxgenetic.py
+++ scripts/pcmaxgenetic.py
@@ -1,16 +1,46 @@
-from pcmaxgreedy import PCMaxGreedy
+from pcmaxlpt import PCMaxLPT
 from pcmaxrotate import PCMaxRotate
 
 class PCMaxGenetic:
-    def __init__(selfself, execTimes, procNum):
+    def __init__(self, execTimes, procNum):
         self.execTimes = execTimes
         self.procNum = procNum
+        self.processorsData = []
+        self.processorsTimes = []
 
-    def solve(self):
-        # find the best greedy solution
-        # swap tasks between longest with shortest (for a required number of repetions or until there's no change in outcome)
-        # swap tasks between more than 2 processors
-        # alongside swaps introduce mutations between longest and random
+    def solve(self, repetitions, mutationsCoeff = 0.1):
+        self.processorsData = self.findBestGreedySolution()
+        self.processorsTimes = [sum(processor) for processor in self.processorsData]
+        for i in range(repetitions):
+            if i % int(1 / mutationsCoeff) == 0:
+                self.mutate()
+            else:
+                self.swapTasksBetweenProcessors()
+                # todo: swap tasks between more than 2 processors
+
+    def findBestGreedySolution(self):
+        greedyAlgorithms = []
+        greedyAlgorithms.append(PCMaxLPT(self.execTimes, self.procNum))
+        greedyAlgorithms.append(PCMaxRotate(self.execTimes, self.procNum))
+        bestCMax = 0
+        bestData = []
+
+        for algorithm in greedyAlgorithms:
+            data, cmax = algorithm.solve()
+            if cmax < bestCMax or not bestData:
+                bestData = data
+
+        return bestData
+
+    def mutate(self):
+        pass
+
+    def swapTasksBetweenProcessors(self):
+        pass
 
     def __del__(self):
         pass
+
+
+genetic = PCMaxGenetic([2,5,2,5,2,20,3,4,1,5,2], 4)
+genetic.solve(50)
