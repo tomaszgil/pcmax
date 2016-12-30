6091dddf604e50436c8dea812ed9b3f2c855b88a
Tomasz Gil <tomaszgil@hotmail.com>
2016-12-30 14:00:00 +0100

    Implement mutation method

--- scripts/pcmaxgenetic.py
+++ scripts/pcmaxgenetic.py
@@ -1,5 +1,6 @@
 from pcmaxlpt import PCMaxLPT
 from pcmaxrotate import PCMaxRotate
+from random import randint
 
 class PCMaxGenetic:
     def __init__(self, execTimes, procNum):
@@ -35,13 +36,24 @@ class PCMaxGenetic:
         return bestData
 
     def mutate(self):
-        print "Mutating..."
-        # mutate longest with random
+        idxOfLongest = self.processorsTimes.index(max(self.processorsTimes))
+        idxOfRandom = randint(0, len(self.processorsData)-1)
+        idxTaskInLongest = randint(0, len(self.processorsData[idxOfLongest])-1)
+        idxTaskInRandom = randint(0, len(self.processorsData[idxOfRandom])-1)
+
+        currentCMax = max(self.processorsTimes)
+        self.processorsData[idxOfLongest][idxTaskInLongest], self.processorsData[idxOfRandom][idxTaskInRandom] = self.processorsData[idxOfRandom][idxTaskInRandom], self.processorsData[idxOfLongest][idxTaskInLongest]
+        self.processorsTimes[idxOfLongest] += (self.processorsData[idxOfRandom][idxTaskInRandom] - self.processorsData[idxOfLongest][idxTaskInLongest])
+        self.processorsTimes[idxOfRandom] += (self.processorsData[idxOfLongest][idxTaskInLongest] - self.processorsData[idxOfRandom][idxTaskInRandom])
+        if currentCMax < max(self.processorsTimes):
+            self.processorsData[idxOfLongest][idxTaskInLongest], self.processorsData[idxOfRandom][idxTaskInRandom] = self.processorsData[idxOfRandom][idxTaskInRandom], self.processorsData[idxOfLongest][idxTaskInLongest]
+            self.processorsTimes[idxOfLongest] += (self.processorsData[idxOfLongest][idxTaskInLongest] - self.processorsData[idxOfRandom][idxTaskInRandom])
+            self.processorsTimes[idxOfRandom] += (self.processorsData[idxOfRandom][idxTaskInRandom] - self.processorsData[idxOfLongest][idxTaskInLongest])
 
     def swapTasksBetweenProcessors(self):
-        idxOfShortest = self.processorsTimes.index(min(self.processorsTimes));
-        idxOfLongest = self.processorsTimes.index(max(self.processorsTimes));
-        processorsConcatenated = self.processorsData[idxOfShortest] + self.processorsData[idxOfLongest];
+        idxOfShortest = self.processorsTimes.index(min(self.processorsTimes))
+        idxOfLongest = self.processorsTimes.index(max(self.processorsTimes))
+        processorsConcatenated = self.processorsData[idxOfShortest] + self.processorsData[idxOfLongest]
         algorithm = PCMaxLPT(processorsConcatenated, 2)
 
         newData, cmax = algorithm.solve()
@@ -50,12 +62,9 @@ class PCMaxGenetic:
         self.processorsTimes[idxOfShortest] = sum(self.processorsData[idxOfShortest])
         self.processorsTimes[idxOfLongest] = sum(self.processorsData[idxOfLongest])
 
-        print self.processorsData
-
     def __del__(self):
         pass
 
 
 genetic = PCMaxGenetic([2,5,2,5,2,10,3,4,1,5,2], 4)
 data, cmax = genetic.solve(10)
-print "Result: ", data, cmax
