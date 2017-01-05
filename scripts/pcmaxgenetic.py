f1e0a2a417cd05631cba2e965e706eae83bc44ad
Tomasz Gil <tomaszgil@hotmail.com>
2017-01-05 13:08:24 +0100

    Changed variable names in mutation and swap

--- scripts/pcmaxgenetic.py
+++ scripts/pcmaxgenetic.py
@@ -39,31 +39,31 @@ class PCMaxGenetic:
         return bestData
 
     def mutate(self):
-        idxOfLongest = self.processorsTimes.index(max(self.processorsTimes))
-        idxOfRandom = randint(0, len(self.processorsData)-1)
-        idxTaskInLongest = randint(0, len(self.processorsData[idxOfLongest])-1)
-        idxTaskInRandom = randint(0, len(self.processorsData[idxOfRandom])-1)
+        l = self.processorsTimes.index(max(self.processorsTimes)) # index of longest processor
+        r = randint(0, len(self.processorsData)-1) # index of random processor
+        tl = randint(0, len(self.processorsData[l])-1) # index of random task in longest processor
+        tr = randint(0, len(self.processorsData[r])-1) # index of random task in random processor
 
         currentCMax = max(self.processorsTimes)
-        self.processorsData[idxOfLongest][idxTaskInLongest], self.processorsData[idxOfRandom][idxTaskInRandom] = self.processorsData[idxOfRandom][idxTaskInRandom], self.processorsData[idxOfLongest][idxTaskInLongest]
-        self.processorsTimes[idxOfLongest] += (self.processorsData[idxOfRandom][idxTaskInRandom] - self.processorsData[idxOfLongest][idxTaskInLongest])
-        self.processorsTimes[idxOfRandom] += (self.processorsData[idxOfLongest][idxTaskInLongest] - self.processorsData[idxOfRandom][idxTaskInRandom])
+        self.processorsData[l][tl], self.processorsData[r][tr] = self.processorsData[r][tr], self.processorsData[l][tl]
+        self.processorsTimes[l] += (self.processorsData[r][tr] - self.processorsData[l][tl])
+        self.processorsTimes[r] += (self.processorsData[l][tl] - self.processorsData[r][tr])
         if currentCMax < max(self.processorsTimes):
-            self.processorsData[idxOfLongest][idxTaskInLongest], self.processorsData[idxOfRandom][idxTaskInRandom] = self.processorsData[idxOfRandom][idxTaskInRandom], self.processorsData[idxOfLongest][idxTaskInLongest]
-            self.processorsTimes[idxOfLongest] += (self.processorsData[idxOfLongest][idxTaskInLongest] - self.processorsData[idxOfRandom][idxTaskInRandom])
-            self.processorsTimes[idxOfRandom] += (self.processorsData[idxOfRandom][idxTaskInRandom] - self.processorsData[idxOfLongest][idxTaskInLongest])
+            self.processorsData[l][tl], self.processorsData[r][tr] = self.processorsData[r][tr], self.processorsData[l][tl]
+            self.processorsTimes[l] += (self.processorsData[l][tl] - self.processorsData[r][tr])
+            self.processorsTimes[r] += (self.processorsData[r][tr] - self.processorsData[l][tl])
 
     def swapTasksBetweenProcessors(self):
-        idxOfShortest = self.processorsTimes.index(min(self.processorsTimes))
-        idxOfLongest = self.processorsTimes.index(max(self.processorsTimes))
-        processorsConcatenated = self.processorsData[idxOfShortest] + self.processorsData[idxOfLongest]
+        s = self.processorsTimes.index(min(self.processorsTimes)) # index of shortest processor
+        l = self.processorsTimes.index(max(self.processorsTimes)) # index of longest processor
+        processorsConcatenated = self.processorsData[s] + self.processorsData[l]
         algorithm = PCMaxLPT(processorsConcatenated, 2)
 
         newData, cmax = algorithm.solve()
-        self.processorsData[idxOfShortest] = newData[0]
-        self.processorsData[idxOfLongest] = newData[1]
-        self.processorsTimes[idxOfShortest] = sum(self.processorsData[idxOfShortest])
-        self.processorsTimes[idxOfLongest] = sum(self.processorsData[idxOfLongest])
+        self.processorsData[s] = newData[0]
+        self.processorsData[l] = newData[1]
+        self.processorsTimes[s] = sum(self.processorsData[s])
+        self.processorsTimes[l] = sum(self.processorsData[l])
 
     def __del__(self):
         pass
