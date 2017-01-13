da431763b78c9b5d0790d24faa4014835208e97b
Tomasz Gil <tomaszgil@hotmail.com>
2017-01-13 19:40:01 +0100

    Fix file generating

--- generatetestinstances.py
+++ generatetestinstances.py
@@ -6,22 +6,22 @@ from instancegenerator import InstanceGenerator
 procNum = 10
 deltaProc = 5
 taskNum = 1000
-deltaTask = 100
+deltaTask = 1000
 minExecTime = 1
 maxExecTime = 1000
 
 g = InstanceGenerator(procNum, taskNum, minExecTime, maxExecTime)
 
-# instances with changing m
+# instances with changing n
 for _ in range(10):
-  g.generateInstance()
+  g.generateOptimumInstance()
   g.save("testing/m" + str(g.procNum) + "n" + str(g.taskNum))
-  g.procNum += procNum
+  g.procNum += deltaNum
 
 g.setParameters(procNum, taskNum, minExecTime, maxExecTime)
 
-# instances with changing n
+# instances with changing m
 for _ in range(10):
-  g.generateInstance()
+  g.generateOptimumInstance()
   g.save("testing/m" + str(g.procNum) + "n" + str(g.taskNum))
-  g.taskNum += taskNum
+  g.taskNum += deltaProc
