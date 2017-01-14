c4f71fb5860f864b8f679d492bf27d0c4aadf5e6
Tomasz Gil <tomaszgil@hotmail.com>
2017-01-14 15:36:47 +0100

    Instances with bigger m

--- solver.py
+++ solver.py
@@ -18,13 +18,20 @@ startTime = time()
 #   ]
 # files = ['instances/m50n1000.txt', 'instances/m25.txt', 'instances/m50.txt']
 
-files = ['instances/testing/m10n1000', 'instances/testing/m10n2000', 'instances/testing/m10n3000',
-    'instances/testing/m10n4000', 'instances/testing/m10n5000', 'instances/testing/m10n6000',
-    'instances/testing/m10n7000', 'instances/testing/m10n8000', 'instances/testing/m10n9000',
-    'instances/testing/m10n10000', 'instances/testing/m10n1000', 'instances/testing/m15n1000',
-    'instances/testing/m20n1000', 'instances/testing/m25n1000', 'instances/testing/m30n1000',
-    'instances/testing/m35n1000', 'instances/testing/m40n1000', 'instances/testing/m45n1000',
-    'instances/testing/m50n1000', 'instances/testing/m55n1000']
+# testing big n values
+# files = ['instances/testing/m10n1000', 'instances/testing/m10n2000', 'instances/testing/m10n3000',
+#     'instances/testing/m10n4000', 'instances/testing/m10n5000', 'instances/testing/m10n6000',
+#     'instances/testing/m10n7000', 'instances/testing/m10n8000', 'instances/testing/m10n9000',
+#     'instances/testing/m10n10000', 'instances/testing/m10n1000', 'instances/testing/m15n1000',
+#     'instances/testing/m20n1000', 'instances/testing/m25n1000', 'instances/testing/m30n1000',
+#     'instances/testing/m35n1000', 'instances/testing/m40n1000', 'instances/testing/m45n1000',
+#     'instances/testing/m50n1000', 'instances/testing/m55n1000']
+
+# testing big m values
+files = ['instances/testing/m100n1100', 'instances/testing/m150n1100',
+    'instances/testing/m200n1100', 'instances/testing/m250n1100', 'instances/testing/m300n1100',
+    'instances/testing/m350n1100', 'instances/testing/m400n1100', 'instances/testing/m450n1100',
+    'instances/testing/m500n1100']
 
 params = [
   { 'instNum': 10, 'iterNum': 100000, 'mutIter': 100, 'normIter': 2 }
